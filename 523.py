8c2d15b6867385629b39ef9df26620417f9ffce9
chubw00 <88913470+chubw00@users.noreply.github.com>
2021-08-13 22:18:17 -0400

    Update 523.py

--- 523.py
+++ 523.py
@@ -187,7 +187,7 @@ def find_closest_embeddings(representation):
 
 
 
-steps=10*len(X)/Batch_size
+steps=2*len(X)/Batch_size
 
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate=rate,
